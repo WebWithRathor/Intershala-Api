bdc3eb22ef0da6adfcb2174d797e35ac07a57c8f
WebWithRathor <devrajrathor143@gmail.com>
2024-06-18 20:14:08 +0530

    job apply

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { homepage, studentsignup, studentsignin, studentsignout, StudentUpdate, CurrentStudent, ResetPassword, studentForgetPassword, ChangePassword, StudentAvatar } = require('../controllers/indexController');
+const { homepage, studentsignup, studentsignin, studentsignout, StudentUpdate, CurrentStudent, ResetPassword, studentForgetPassword, ChangePassword, StudentAvatar, studentApplyJob, studentApplyInternship } = require('../controllers/indexController');
 const { isAuthenticated } = require('../middlewares/Auth');
 
 router.get('/', isAuthenticated, homepage);
@@ -22,5 +22,9 @@ router.post('/student/changepassword', isAuthenticated, ChangePassword);
 router.post('/student/update', isAuthenticated, StudentUpdate);
 
 router.post('/student/avatar', isAuthenticated, StudentAvatar);
+
+router.post('/student/applyJob/:jobid', isAuthenticated, studentApplyJob);
+
+router.post('/student/applyInternship/:internshipid', isAuthenticated, studentApplyInternship);
 
 module.exports = router;
