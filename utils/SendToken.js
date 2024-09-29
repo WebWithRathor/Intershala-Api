37daeebe561bf34b7a29450e0954d2d74ec2aea7
WebWithRathor <devrajrathor143@gmail.com>
2024-09-29 22:13:27 +0530

    .

--- utils/SendToken.js
+++ utils/SendToken.js
@@ -2,7 +2,7 @@ exports.SendToken = (Student,statuCode,res)=>{
     const token = Student.generatejwt();
     const options={
         expires: new Date(Date.now() + 24*60*60*1000),
-        httpOnly: true,
+        // httpOnly: true,
         secure:true
     }
 
