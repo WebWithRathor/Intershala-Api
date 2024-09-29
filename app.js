2d07fdfe380191e5a2dce780bf841f7744011c51
WebWithRathor <devrajrathor143@gmail.com>
2024-09-29 22:50:33 +0530

    .

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ app.use(cors({
 }));
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', "https://intershala-api.onrender.com");
     res.header('Access-Control-Allow-Headers', true);
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
